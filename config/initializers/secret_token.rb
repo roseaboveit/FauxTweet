d5b0a9a1c5145ddaac9f03266850b6b16c577044
Whitney Levis <ms.whitney.levis@gmail.com>
2014-01-12 11:13:35 -0800

    first commit with testing environment set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FauxTweet::Application.config.secret_key_base = '3de61e6ce7791a4e9b4a7e5f81da7f8e7202866f5872896a1b22d5f2a3bfc8494b131717533e9c3670f123012d0f322d8914f1680b5367033f6b790c86d1038e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FauxTweet::Application.config.session_store :cookie_store, key: '_FauxTweet_session'
